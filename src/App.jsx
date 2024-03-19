79f7d7214441ee8bbc8236194be02fa818bcf2d7
TI Sabbir <tisabbir0@gmail.com>
2024-03-19 11:08:10 +0600

    navbar from daisy ui is added, short banner is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,13 @@
-import { useState } from "react";
 import "./App.css";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Hello from "./components/HelloWorld/Hello";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className=" p-4 m-4 text-2xl font-bold text-center">Vite + React</h1>
-      <h1 className="p-4 m-5 text-7xl bg-red-500">Vite + React</h1>
-      <div className="card">
-        <button
-          className="btn btn-ghost"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Navbar></Navbar>
+      <Hello></Hello>
+    </div>
   );
 }
 
---
+++ src/components/HelloWorld/Hello.jsx
@@ -0,0 +1,13 @@
+const Hello = () => {
+  return (
+    <div>
+      <div>
+        <h1 className="text-center bg-lime-300 rounded-xl p-4 m-4 text-3xl font-semibold">
+          Welcome to Price Options
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Hello;
